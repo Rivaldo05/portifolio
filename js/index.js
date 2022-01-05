945019f64cba6379f2dde32207bfcee9178a67ca
Rivaldo05 <67082413+Rivaldo05@users.noreply.github.com>
2022-01-05 14:37:42 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let skills = [
+    {lang : "Python", porcent: 60, imgName: "Imagens/python.png"},
+    {lang :"Html", porcent: 80, imgName: "Imagens/html5.png"},
+    {lang : "Css", porcent: 70, imgName: "Imagens/css.png"},
+    {lang : "JavaScript" , porcent: 30, imgName:"Imagens/javascript.png"}
+];
+
+const select = (el)=> document.querySelector(el)
+const selectall = (el)=> document.querySelectorAll(el)
+
+skills.map((lang, langIndex) => {
+    SkillArea = select('.conteinerImg').cloneNode(true);/* o obj a ser clonado tem que estar em uma area que não interfira bo doc*/
+    /*SkillArea  = document.createElement('div')
+    SkillArea.classList.add('conteinerImg')*/
+    conteiner = `
+    <div class="imgCont">
+        <img src="${lang.imgName}" alt="${lang.lang}">
+        <div class="progress">
+            <div class="porcentProgress" ></div>
+        </div>
+    </div>`
+
+    SkillArea.innerHTML = conteiner
+
+    conteinerSkill = select('.conteinerSkills').append(SkillArea)
+})
+
+        /* ANIMAÇÃO DA BARRA DE PROGRESSO */
+setTimeout(() => {
+   progressBar = selectall('.progress .porcentProgress')
+    for(i=0; i<progressBar.length; i++){
+        progressBar[i].style.width = `${skills[i].porcent}%`
+    }
+    }, 100);
+
+
+  
+    
+     
