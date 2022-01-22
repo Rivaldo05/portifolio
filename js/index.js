16bf1a01cdda01c086dc47e0bd37a09d8afd0677
Rivaldo05 <67082413+Rivaldo05@users.noreply.github.com>
2022-01-22 01:27:27 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,23 +1,62 @@
 let skills = [
-    {lang : "Python", direcao_anime : "left", imgName: "Imagens/python.png"},
-    {lang :"Html", direcao_anime: "righ", imgName: "Imagens/html5.png"},
-    {lang : "Css", direcao_anime: "left", imgName: "Imagens/css.png"},
-    {lang : "JavaScript" , direcao_anime: "righ", imgName:"Imagens/javascript.png"}
+    {lang : "Python", direcao_anime : "left", nivel: "Básico" , imgName: "Imagens/python.png"},
+    {lang :"Html", direcao_anime: "righ", nivel: "Avançado", imgName: "Imagens/html5.png"},
+    {lang : "Css", direcao_anime: "left", nivel: "Avançado" , imgName: "Imagens/css.png"},
+    {lang : "JavaScript" , direcao_anime: "righ", nivel: "Básico" , imgName:"Imagens/javascript.png"}
 ];
 
 const select = (el)=> document.querySelector(el)
 const selectall = (el)=> document.querySelectorAll(el)
+const classAnimate = "animate"
 
 skills.map((lang, langIndex) => {
-    /*SkillArea = select('.conteinerImg').cloneNode(true);/* o obj a ser clonado tem que estar em uma area que não interfira bo doc*/
     SkillArea  = document.createElement('div')
     SkillArea.classList.add('conteinerImg')
     SkillArea.setAttribute("data-anim", `${lang.direcao_anime}`)
     conteiner = `
       <img src="${lang.imgName}" alt="${lang.lang}">
-      <span class="lang-name">${lang.lang}</span>
+      <span class="lang-name">${lang.lang} - ${lang.nivel}</span>
     `
     SkillArea.innerHTML = conteiner
     conteinerSkill = select('.conteinerSkills').append(SkillArea)
 })
 
+const dataAtributes = selectall('[data-anim]')
+
+function animeScrool(){
+  const alturaTela = window.scrollY + ((window.innerHeight * 3)/3)
+
+  dataAtributes.forEach((elemento_data)=>{
+    if (alturaTela > elemento_data.offsetTop){
+      elemento_data.classList.add(classAnimate)
+      console.log(1);
+    }else{
+      elemento_data.classList.remove(classAnimate)
+      console.log(2);
+    }
+  })
+}
+
+// função loaddash pega da internet
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+animeScrool()
+if(dataAtributes.length){
+  window.addEventListener('scroll',
+    debounce(()=>{
+    animeScrool()
+  },100)
+)}
